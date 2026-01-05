169dfdc7d7f3acd5f1b1c40b11ed32ea702b393c
jordyH54 <jordy_hdez@hotmail.com>
2026-01-05 14:46:22 -0600

    se agrega ejercicio 5

--- task5.js
+++ task5.js
@@ -1,8 +1,12 @@
-
-
 // Type your code below this line!
 
+const total = Number(process.argv[2])
+const names = []
 
+for (let i = 0; i < total; i++) {
+  names.push(process.argv[3 + i])
+}
+
+console.log(names)
 
 // Type your code above this line!
-
